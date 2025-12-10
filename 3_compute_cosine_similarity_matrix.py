64832ce5d3a777d854f29b4ae3b29b7fd91b94d6
Ray Zhu <rzhu@lambda5.cels.anl.gov>
2025-12-09 20:59:52 -0600

    update 3_.py

--- 3_compute_cosine_similarity_matrix.py
+++ 3_compute_cosine_similarity_matrix.py
@@ -1,30 +1,11 @@
-import os
 import faiss
-from typing import Union
 import numpy as np
-import pandas as pd
-
-def load_faiss_index(filename: str) -> Union[faiss.Index, None]:
-    """Loads a FAISS index from a specified file path."""
-    if os.path.exists(filename):
-        try:
-            index = faiss.read_index(filename)
-            print(f"Successfully loaded existing FAISS index from: {filename}")
-            return index
-        except Exception as e:
-            print(f"Error loading FAISS index: {e}")
-            return None
-    return None
 
 def l2_normalize_vectors(vectors: np.ndarray) -> np.ndarray:
     """L2 normalizes a set of vectors (making their magnitude 1.0)."""
     # faiss.normalize_L2 is a highly optimized, in-place operation
     faiss.normalize_L2(vectors)
     return vectors
-
-index = load_faiss_index('reaction_embeddings.faiss')
-
-rxn_data = pd.read_csv('rxn_data.csv')
 
 all_idx_i = []
 all_idx_j = []
